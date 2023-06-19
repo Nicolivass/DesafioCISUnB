23215d156a9de0da72db69a4a0d00f04bf758b66
Nicole Sena <nicjlucasn@gmail.com>
2023-06-19 18:18:32 -0300

    evolução nas questoes

--- README.txt
+++ README.txt
@@ -5,11 +5,11 @@ IEEE Computational Intelligence Society, UnB.
 Visa avaliar as habilidades dos candidatos em manipular,
 visualizar e interpretar grandes volumes de dados.
 
-Foi utilizada a linguagem R para manipulação dos Datasets e Conclusão das questões solicitadas.
+Foi utilizada a linguagem R para manipulação dos Datasets e conclusão das questões solicitadas.
 
-Para saber mais acesse <code>Apresentação do Desafio.docx<code>
+Para saber mais acesse o arquivo "Apresentação do Desafio.docx"
 
-Questôes:
+Resolução das questôes:
 
 1. No dataset human_freedom, existem alguns valores faltantes. Antes de
 começar a manipular os dados, trate essas informações e descreva
@@ -19,13 +19,129 @@ R: Para tratar os dados faltantes eu usei a técnica de adicionar o valor da mé
 se a variação dos valores for baixa ou se a variável não possuir muita importância é possível substituir as missing values pelo valor da média.
 
 
-# tratando as colunas pf_rol_procedural, pf_rol_civil, pf_rol_criminal alterando os valores faltantes pela média
 
-<code>hfi$pf_rol_procedural <- ifelse(is.na(hfi$pf_rol_procedural), mean(hfi$pf_rol_procedural, na.rm = TRUE), hfi$pf_rol_procedural)<code>
 
-<code>hfi$pf_rol_civil <- ifelse(is.na(hfi$pf_rol_civil), mean(hfi$pf_rol_civil, na.rm = TRUE), hfi$pf_rol_civil)<code>
-
-<code>hfi$pf_rol_criminal <- ifelse(is.na(hfi$pf_rol_criminal), mean(hfi$pf_rol_criminal, na.rm = TRUE), hfi$pf_rol_criminal)<code>
 
 2. Calcule o número total de medalhas recebido por cada país e adicione essa coluna no dataset:
+
+O dataset já tinha o total, mas eu recalculei usando a rowSums, tive que apagar a linha de hongkong pois o dataset colocou o valor "china" na coluna de gold medal e atrapalhou. 
+Tambem tive que transformar tudo em numerico pois estava dando erro.
+
+
+
+
+3. Seguindo as regras oficiais das Olimpíadas, obtenha o ranking dos
+países. Visualize, também, os dez melhores países com base no total de
+medalhas;
+
+Também ja tinha a coluna rank no dataset, mas eu calculei de novo usando a função rank.
+Depois puxei os dez primeiros paises.
+
+
+
+
+4.Obtenha a média de alfabetização por região e a represente
+graficamente: 
+
+R: Para calcular a média usei a função mean do R em cada região.
+
+Média de alfabetização por região:
+ 
+                 Region Literacy (%)
+1  ASIA (EX. NEAR EAST)     79.55357
+2               BALTICS     99.73333
+3   C.W. OF IND. STATES     98.72500
+4        EASTERN EUROPE     97.08889
+5   LATIN AMER. & CARIB     90.65455
+6             NEAR EAST     79.52143
+7       NORTHERN AFRICA     67.24000
+8      NORTHERN AMERICA     97.75000
+9               OCEANIA     88.83529
+10   SUB-SAHARAN AFRICA     62.51000
+11       WESTERN EUROPE     98.39130
+
+Gráfico no arquivo "Grafico_questao_4.png"
+
+
+
+
+
+5.Liste os cinco países com maior número de celulares a cada mil habitantes. Faça uma representação gráfica;
+
+R: [1] "Monaco"       "UnitedStates" "Gibraltar"    "Bermuda"      "Guernsey"   
+
+Ordenei a coluna "Phones (per 1000)" em ordem decrescente, e depois peguei os 5 primeiros dessa lista.
+
+Para construir o gráfico separei as colunas "Country" e "Phones (per 1000)" em um data frame.
+Gráfico no arquivo "Grafico_questao_5.png".
+
+
+
+
+
+6. Obtenha os dados do dataset human_freedom apenas para o ano de
+2018;
+
+R: Gerei um subset filtrando pelo ano
+
+
+
+
+
+7. Mostre a evolução do hf_score (human freedom score) do Brasil em
+função do tempo;
+
+R: primeiro eu criei um subset apenas com as colunas "countries", "hf_score" e "year". Depois
+eu filtrei pelo país "Brazil" e odernei pelo ano. Depois criei um gráfico. Gráfico no arquivo "Grafico_questao_7.png".
+
+
+
+
+
+8. Mostre qual é a região com a maior média populacional;
+
+R: ASIA (EX. NEAR EAST)  131713651
+
+Primeiro eu fiz um agregado com as médias por região, depois eu ordenei do maior pro menor e peguei o primeiro.
+
+
+
+
+
+9. Com base na análise dos dados, é possível afirmar que países com maior
+população tendem a ter mais pessoas saindo ou entrando no país (maior
+fluxo migratório)?
+
+R: de acordo com os dados do data frame "migracao_paises_ordenado" foi possível concluir que quanto maior a população menor a taxa líquida de migração, porém saber sobre o fluxo fica inconclusivo.
+
+
+
+
+
+10. Caso o número de medalhas de bronze e de ouro fossem trocadas, qual
+seria o ranking dos países?
+
+Como o ranking foi feito de acordo com o total de medalhas, trocar as colunas não fez diferença
+
+
+
+
+
+11. Normalize os valores dos datasets;
+
+
+
+
+
+12. É possível observar correlação entre os índices de natalidade e de
+mortalidade? Justifique o resultado encontrado;
+
+R: Nessa questão não fiz nenhum código, Apenas ordenando a coluna de GDP(PIB) observei que quanto maior o pib menor a diferença entre esses indices, e quanto menor o pib, maior a natalidade e menor a mortalidade.
+
+
+
+
+13. É possível observar correlação entre a generosidade da população e o
+índice de agricultura dos países? Justifique o resultado encontrado;
+
 
--- script.r
+++ script.r
@@ -1,9 +1,4 @@
-
-# tratando as colunas pf_rol_procedural, pf_rol_civil, pf_rol_criminal alterando os valores faltantes pela média
-hfi$pf_rol_procedural <- ifelse(is.na(hfi$pf_rol_procedural), mean(hfi$pf_rol_procedural, na.rm = TRUE), hfi$pf_rol_procedural)
-hfi$pf_rol_civil <- ifelse(is.na(hfi$pf_rol_civil), mean(hfi$pf_rol_civil, na.rm = TRUE), hfi$pf_rol_civil)
-hfi$pf_rol_criminal <- ifelse(is.na(hfi$pf_rol_criminal), mean(hfi$pf_rol_criminal, na.rm = TRUE), hfi$pf_rol_criminal)
-
+#convertendo os csv em rda
 install.packages("readxl")
 library(readxl)
 medals <- read_xlsx("medals.xlsx")
@@ -14,3 +9,149 @@ save(geral, file = "geral.rda")
 
 hapiness <- read_xlsx("hapiness.xlsx")
 save(hapiness, file = "hapiness.rda")
+
+freedom <- read_xlsx("freedom.xlsx")
+save(freedom, file = "freedom.rda")
+
+# (Questão 1) tratando as colunas pf_rol_procedural, pf_rol_civil, pf_rol_criminal alterando os valores faltantes pela média
+freedom$pf_rol_procedural <- ifelse(is.na(freedom$pf_rol_procedural), mean(freedom$pf_rol_procedural, na.rm = TRUE ), freedom$pf_rol_procedural)
+freedom$pf_rol_civil <- ifelse(is.na(freedom$pf_rol_civil), mean(freedom$pf_rol_civil, na.rm = TRUE), freedom$pf_rol_civil)
+freedom$pf_rol_criminal <- ifelse(is.na(freedom$pf_rol_criminal), mean(freedom$pf_rol_criminal, na.rm = TRUE), freedom$pf_rol_criminal)
+
+
+
+
+
+# (Questão 2) número total de medalhas 
+medals <- medals[-49, ]#essa linha estáva dando problema pois separou HongKong de China
+colunas_para_total <- c("Gold Medal", "Silver Medal", "Bronze Medal")
+medals[, colunas_para_total] <- sapply(medals[, colunas_para_total], as.numeric)#tive que transformar tudo em numerico
+medals$TotalCalculado <- rowSums(medals[, colunas_para_total], na.rm = TRUE)
+
+
+
+
+# (Questão 2) obtendo o ranking dos países. 
+medals$Ranking <- rank(-medals$TotalCalculado, ties.method = "min")
+melhores_paises <- head(medals, 10)
+print(melhores_paises$Country)
+
+
+
+
+# (Questão 4) Filtrando e calculando a média
+media_alfabet_por_regiao <- aggregate(`Literacy (%)` ~ Region, data = geral, FUN = function(x) mean(x, na.rm = TRUE))
+print(media_alfabet_por_regiao)
+# Mudar o nome da coluna
+colnames(media_alfabet_por_regiao)[2] <- "Literacy"
+
+#Grafico Q4
+install.packages("ggplot2")
+library(ggplot2)
+#data frame
+dadosGrafico <- data.frame(media_alfabet_por_regiao)
+#conjunto de dados
+grafico <- ggplot(data = dadosGrafico)
+#espicificar as variáveis
+grafico <- grafico + aes(x = Region, y = Literacy)
+#tipo de grafico
+grafico <- grafico + geom_bar(stat = "identity")
+print(grafico)
+
+
+
+# (Questão 5) cinco países com maior número de celulares
+
+# Ordenar a coluna
+paises_c_mais_celulares <- geral[order(geral$`Phones (per 1000)`, decreasing = TRUE), ]
+# Os cinco países com maior número de celulares
+primeiros_cinco <- head(paises_c_mais_celulares, 5)
+print(primeiros_cinco$Country)
+
+#Grafico Q5
+#data frame
+colunas_grafico <- subset(primeiros_cinco, select = c("Country", "Phones (per 1000)"))
+#conjunto de dados
+grafico <- ggplot(data = colunas_grafico)
+#espicificar as variáveis
+grafico <- grafico + aes(x = Country, y = `Phones (per 1000)`)
+#tipo de grafico
+grafico <- grafico + geom_bar(stat = "identity")
+print(grafico)
+
+
+
+
+
+
+# (Questão 6) dados human_freedom apenas para 2018
+
+dados_2018 <- freedom[freedom$year == 2018, ]
+print(dados_2018)
+
+
+
+
+# (Questão 7) evolução do hf_score do Brasil
+hf_score <- subset(freedom, select = c("countries", "hf_score", "year"))
+#Filtrar pelo brasil
+hf_score_Brazil <- hf_score[hf_score$countries == "Brazil", ]
+#Ordenar
+hf_score_Brazil_ordenado <- hf_score_Brazil[order(hf_score_Brazil$year), ]
+
+#Criar um gráfico 
+
+#conjunto de dados
+grafico <- ggplot(data = hf_score_Brazil_ordenado)
+#espicificar as variáveis
+grafico <- grafico + aes(x = year, y = hf_score)
+#tipo de grafico
+grafico <- grafico + geom_bar(stat = "identity")
+#definir intervalo de anos
+intervalo <- c(2007, 2019) 
+grafico <- grafico + coord_cartesian(xlim = intervalo)
+print(grafico)
+
+
+
+
+
+# (Questão 8) a região com a maior média populacional
+media_por_regiao <- aggregate(Population ~ Region, data = geral, FUN = mean)
+media_por_regiao_ordenado <- media_por_regiao[order(media_por_regiao$Population, decreasing = TRUE), ]
+print(primeiro <- head(media_por_regiao_ordenado, 1))
+
+
+
+
+
+# (Questão 9) países com maior população tendem a ter maior fluxo migratório
+migracao_paises <- subset(geral, select = c("Country", "Population", "Net migration"))
+migracao_paises_ordenado <- migracao_paises[order(migracao_paises$Population, decreasing = TRUE), ]
+
+
+
+
+
+
+# (Questão 10) qual seria o ranking dos países se medalhas de bronze e de ouro fossem trocadas
+medals$aux <- medals$`Gold Medal`
+medals$`Gold Medal` <- medals$`Bronze Medal`
+colunas_para_total <- c("aux", "Silver Medal", "Bronze Medal")
+medals[, colunas_para_total] <- sapply(medals[, colunas_para_total], as.numeric)
+medals$TotalCalculado <- rowSums(medals[, colunas_para_total], na.rm = TRUE)
+medals$Ranking_trocado <- rank(-medals$TotalCalculado, ties.method = "min")
+melhores_paises <- head(medals, 10)
+print(melhores_paises$Country)
+
+
+
+#(Questão 11) Normalizando os valores dos datasets
+
+
+
+
+
+
+#(Questão 12) correlação entre os índices de natalidade e de mortalidade
+
